deae646a3f43aeb63e22b5e666ae181ceae00761
Joe <jb.barrientos@yahoo.com>
2022-04-13 19:44:24 -0300

    Fused two handlers into the generic Userhandler to manage everything related to the user api endpoint.

--- components/api/UserHandler.ts
+++ components/api/UserHandler.ts
@@ -1,21 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { IPointsCharge } from "./PointsCharge"
 import { IUserFetch } from "./UserFetch"
 
 export class UserHandler {
-    url: string
-    token: string
-    fetchUser: IUserFetch
-    constructor(url: string, token: string, fetchUser: IUserFetch) {
-        this.url = url
+    points: string;
+    user: string;
+    token: string;
+    userFetch: IUserFetch;
+    pointsCharge: IPointsCharge;
+    constructor(points: string, user: string, token: string, userFetch: IUserFetch, pointsCharge: IPointsCharge) {
+        this.points = points
+        this.user = user
         this.token = token
-        this.fetchUser = fetchUser
+        this.userFetch = userFetch
+        this.pointsCharge = pointsCharge
+    }
+    handlePointCharge(req: NextApiRequest, res: NextApiResponse) {
+        return new Promise<void>(resolve => {
+            let { amount } = JSON.parse(req.body)
+            this.pointsCharge.chargePoints(this.points, amount, this.token)
+                .then(data => res.status(200).json(data))
+                .catch(err => res.status(400).json(err))
+                .finally(resolve)
+        })
+    }
+    handleInvalidMethod(req: NextApiRequest, res: NextApiResponse) {
+        return new Promise<void>(resolve => {
+            res.status(405).send("Method not allowed")
+            resolve()
+        })
     }
     handleFetchUser(req: NextApiRequest, res: NextApiResponse) {
         return new Promise<void>(resolve => {
-            this.fetchUser.get(this.url, this.token)
+            this.userFetch.get(this.user, this.token)
                 .then(data => res.json(data))
                 .catch(err => res.json({ error: err }))
                 .finally(resolve)
         })
     }
 }
+    
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,18 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { PointHandler } from '../../components/api/PointHandler'
 import { PointsCharge } from '../../components/api/PointsCharge'
 import { UserFetch } from '../../components/api/UserFetch'
 import { UserHandler } from '../../components/api/UserHandler'
 
-let user = new UserHandler(process.env.USERURL, process.env.USERTOKEN, new UserFetch())
-let points = new PointHandler(process.env.POINTSURL, process.env.USERTOKEN, new PointsCharge())
+let user = new UserHandler(
+    process.env.POINTSURL,
+    process.env.USERURL,
+    process.env.USERTOKEN,
+    new UserFetch(),
+    new PointsCharge()
+)
+
 
 export default async function userHandler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     if (req.method === 'GET') return user.handleFetchUser(req, res)
-    else if (req.method === "POST") return points.handlePointCharge(req, res)
-    else return points.handleInvalidMethod(req, res)
-
+    else if (req.method === "POST") return user.handlePointCharge(req, res)
+    else return user.handleInvalidMethod(req, res)
 }
