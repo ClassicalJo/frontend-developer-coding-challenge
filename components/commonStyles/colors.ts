9752d6e16494e23398a6d01ae62f0cc6d5d20524
Joe <jb.barrientos@yahoo.com>
2022-04-14 13:39:31 -0300

    Removed unused function
    I was going to use it to validate hex strings, but turns out typescript cant exactly do that yet without some serious hacky turnarounds.

--- components/commonStyles/colors.ts
+++ components/commonStyles/colors.ts
@@ -1,12 +1,3 @@
-function hexToRGB(hex: string, alpha: number) {
-    const r = parseInt(hex.slice(1, 3), 16)
-    const g = parseInt(hex.slice(3, 5), 16)
-    const b = parseInt(hex.slice(5, 7), 16)
-    if (alpha) return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-}
-
-
 const colors = {
     neutrals: {
         "900": "#252F3D",
