0f51ce3e9547d24c98d79a8ea29f60dc0dffcf28
Joe <jb.barrientos@yahoo.com>
2022-04-19 05:04:40 -0300

    Removed ProductList as replaced with Products.
    Commentated until full functionality is established.

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -6,6 +6,7 @@ import { Product, EffectUserData } from "../types"
 import { StyledMain, StyledMainWrapper } from "./styles"
 import NavBar from "../nav"
 import Browse from "../browse"
+import Products from "../products"
 
 interface AppProps {
     productsData: Product[]
@@ -29,8 +30,9 @@ export default function Main({ productsData }: AppProps): JSX.Element {
                 <NavBar />
                 <Hero />
                 <Browse />
+                <Products products={productsData} />
                 <UserList userData={userData} refreshUserData={fetchUser} />
-                <ProductList productsData={productsData} userData={userData} refreshUserData={fetchUser} />
+                {/* <ProductList productsData={productsData} userData={userData} refreshUserData={fetchUser} /> */}
             </StyledMainWrapper>
         </StyledMain>
     )
