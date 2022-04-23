ac5e9ddc3524dade423aabc9ec1b4878f7a6590a
Joe <jb.barrientos@yahoo.com>
2022-04-23 16:02:37 -0300

    Replaced with working component

--- components/nav/Balance.tsx
+++
@@ -1,9 +0,0 @@
-interface AppProps {
-    points: number
-    className?: string
-}
-export default function Balance({ points, className }: AppProps): JSX.Element {
-    return (
-        <p className={className}>{points.toLocaleString()}</p>
-    )
-}
