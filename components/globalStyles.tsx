bae535c768053baf3406243f906ecf3df9278a36
Joe <jb.barrientos@yahoo.com>
2022-04-24 03:19:29 -0300

    Adding the OS outline, adding animations

--- components/globalStyles.tsx
+++ components/globalStyles.tsx
@@ -1,14 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 import normalize from "styled-normalize";
-import { appear, disappear, expandUserCard, shrinkUserCard } from "./commonStyles/animations";
+import { appear, disappear, expandUserCard, rotateDown, rotateUp, shrinkUserCard } from "./commonStyles/animations";
 
 export default createGlobalStyle`
     ${normalize}
     *{
         margin: 0;
         padding: 0;
-        outline:0;
-        box-sizing:border-box;
+        box-sizing:border-box;   
     }
     #root{
         margin:0 auto;
@@ -21,4 +20,6 @@ export default createGlobalStyle`
     ${shrinkUserCard}
     ${appear}
     ${disappear}
+    ${rotateUp}
+    ${rotateDown}
 `
