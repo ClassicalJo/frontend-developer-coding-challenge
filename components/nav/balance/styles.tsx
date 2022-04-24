fa38b49b1d686f70af564bf2e877c3042539a784
Joe <jb.barrientos@yahoo.com>
2022-04-24 03:30:11 -0300

    Reworking styles to better fit figma schematics

--- components/nav/balance/styles.tsx
+++ components/nav/balance/styles.tsx
@@ -14,18 +14,21 @@ interface UserCardSelectPointsProps {
 }
 export const StyledBalance = styled.div`
     border:1px solid ${colors.neutrals["300"]};
-    display: flex;
+    ${mixins.centerDiv}
     border-radius:16px;
-    padding: 6px;
+    box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.08);
     position:relative;
-    
+    flex:1;
+    user-select: none;
+    cursor: pointer;
 `
 export const StyledBalancePoints = styled.div`
     ${textStyles.mobile.texts.l1.default}
     ${mixins.centerDiv}
     ${mixins.gradientText(colors.brand.default)}
+    pointer-events: none;
 
-    @media (min-width: ${breakpoints.desktop}){
+    @media (min-width: ${breakpoints.tablet}){
         ${textStyles.desktop.texts.l1.default}
     }
 `
@@ -33,28 +36,49 @@ export const StyledBalanceWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-around;
+    
 `
 
 export const StyledBalanceLogo = styled.div`
     ${mixins.centerDiv}
-    margin:0 15px;
+    margin:8px;
+    margin-left: 16px;
     width:24px;
     height:24px;
 
-    @media (min-width: ${breakpoints.desktop}){
+    @media (min-width: ${breakpoints.tablet}){
         width: 32px;
         height: 32px;
     }
 `
 
-export const rotatedLogo = (deg: number) => styled.div`
-    transform: rotate(${deg}deg);
+interface ChevronProps {
+    rotate: Boolean;
+    firstRender: Boolean;
+}
+export const StyledChevron = styled(UnstyledNextImage) <ChevronProps>`
+    margin:12px 16px;
+    ${props => `
+        width: ${props.width}px;
+        height: ${props.height}px;
+    `}
+    ${props => {
+        if (props.firstRender) return `
+            transform: rotate(90deg);
+        `
+        else if (props.rotate) return `
+            animation: rotateDown .3s forwards;
+        `
+        else return `
+            animation: rotateUp .3s forwards;
+    
+        `
+    }}
 `
-export const StyledChevronDown = rotatedLogo(90)
-export const StyledChevronUp = rotatedLogo(270)
 
 export const StyledUserCardContainer = styled.div<UserCardProps>`
     position: absolute;
+    cursor: default;
     border-radius: 16px;
     border: 1px solid ${colors.neutrals["300"]};
     width:100vw;
@@ -90,13 +114,13 @@ export const StyledUserCardSection = styled.div`
 `
 export const StyledUserCardPointsWrapper = styled.div`
     display: flex;
-    gap: 3px;
+    gap: 4px;
 `
 export const StyledUserCardPointsSelectWrapper = styled.div<UserCardSelectPointsProps>`
     ${mixins.centerDiv}
-
+    cursor: pointer;
     flex:1;
-    padding: 5px 0;
+    padding: 4px 0;
     border-radius: 12px;
     ${props => {
         if (props.current) return `
@@ -110,14 +134,12 @@ export const StyledUserCardPointsSelectWrapper = styled.div<UserCardSelectPoints
 `
 export const StyledUserCardPointsSelect = styled.div<UserCardSelectPointsProps>`
     ${textStyles.desktop.texts.l1.default}
-    
     ${props => {
         if (props.current) return `
         color: white;
         `
         return mixins.gradientText(colors.brand.default)
-    }
-    }
+    }}
 `
 export const StyledUserCardPointsButton = styled.div`
     background: ${colors.brand.default};
@@ -127,7 +149,17 @@ export const StyledUserCardPointsButton = styled.div`
     ${textStyles.desktop.texts.l1.default}
     ${mixins.centerDiv}
     gap:5px;
+    cursor: pointer;
+`
+export const StyledUserCardPointsButtonProcessing = styled.div`
+    background: ${colors.specials.illustrationBG};
+    color: white;
+    padding:10px 0;
+    border-radius: 16px;
+    ${textStyles.desktop.texts.l1.default}
+    ${mixins.centerDiv}
     
+    cursor: pointer;
 `
 export const StyledUserCardPointsButtonIcon = styled(UnstyledNextImage)`
     height:24px;
@@ -137,33 +169,39 @@ export const StyledAerocard = styled.div`
     display: flex;
     position: relative;
     background: ${colors.neutrals["900"]};
+    color: ${colors.neutrals["100"]};
     height: 148px;
     flex-direction: column;
     justify-content: space-between;
-    padding: 15px 15px 20px 15px;
+    padding: 16px;
     border-radius: 8px;
     margin-bottom:10px;
     overflow: hidden;
+    ${textStyles.mobile.texts.l1.default}
+    @media (min-width: ${breakpoints.tablet}) {
+        ${textStyles.desktop.texts.l1.default}    
+    }
 `
 export const StyledAerocardTitleContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    color: ${colors.neutrals["100"]};
-    ${textStyles.desktop.texts.l1.default}
-`
-export const StyledAerocardTitle = styled.div`
-    ${textStyles.desktop.texts.l1.default}  
+    
 `
 export const StyledAerocardIcon = styled(UnstyledNextImage)`
-    width:24px;
-    height: 24px;
+    width: 20px;
+    height: 20px;
+
+    @media (min-width: ${breakpoints.tablet}) {
+        width:  24px;
+        height: 24px;    
+    }
 `
 export const StyledAerocardUsername = styled.p`
-    ${textStyles.desktop.texts.l2.default}
-`
-export const StyledAerocardDate = styled.p`
-    ${textStyles.desktop.texts.l1.default}
+    ${textStyles.mobile.texts.l2.default}
+    @media (min-width: ${breakpoints.tablet}) {
+        ${textStyles.desktop.texts.l2.default}    
+    }
 `
 
 export const StyledAerocardWaves = styled(AerocardWaves)`
@@ -173,5 +211,4 @@ export const StyledAerocardWaves = styled(AerocardWaves)`
     left: 0px;
     opacity:.1;
     transform: rotate(-4.26deg);
-
 `
