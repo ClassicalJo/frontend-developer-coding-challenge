85e0c4e50dbd1039f5e970c9b7c4af083742db94
Joe <jb.barrientos@yahoo.com>
2022-04-24 21:18:01 -0300

    Fixed rotation with correct arguments

--- components/nav/balance/AerocardWaves.tsx
+++ components/nav/balance/AerocardWaves.tsx
@@ -11,7 +11,6 @@ let CustomWave = ({ lineWidth }: WaveProps) => (
             d="M1 13.5C32.3333 0 63.6667 0 95 13.5C126.333 27 157.667 27 189 13.5C220.333 0 251.667 0 283 13.5C314.333 27 345.667 27 377 13.5C408.333 0 439.667 0 471 13.5C502.333 27 533.667 27 565 13.5"
             stroke="#F5F9FF" strokeWidth={lineWidth}
         />
-
     </svg>
 )
 
@@ -36,7 +35,7 @@ function AerocardWaves({ className }: StyledProplessComponent): JSX.Element {
                     width={566}
                     height={27}
                     y={waveMargin * i + yOffSet}
-                    transform="rotate(4.26deg)"
+                    transform="rotate(4.26 0 0)"
                 >
                     <animateTransform
                         attributeName='transform'
