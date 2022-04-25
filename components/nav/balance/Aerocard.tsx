fc0066009c2a51ae793df9c30f0b5e8f92d6876a
Joe <jb.barrientos@yahoo.com>
2022-04-24 21:19:19 -0300

    Small date fix

--- components/nav/balance/Aerocard.tsx
+++ components/nav/balance/Aerocard.tsx
@@ -1,5 +1,5 @@
 import { UserData } from "../../types";
-import { StyledAerocard, StyledAerocardIcon, StyledAerocardTitleContainer, StyledAerocardUsername, StyledAerocardWaves } from "./styles";
+import { StyledAerocard, StyledAerocardIcon, StyledAerocardTitleContainer, StyledAerocardUsername, StyledAerocardWaves} from "./styles";
 import aeropay from '../../../assets/icons/aeropay-2.svg'
 
 interface AppProps {
@@ -10,15 +10,15 @@ export default function Aerocard({ userData }: AppProps): JSX.Element {
     let date = new Date(userData.createDate)
     return (
         <StyledAerocard>
-            <StyledAerocardWaves/>
-            <StyledAerocardTitleContainer>
-                <p>Aerocard</p>
-                <StyledAerocardIcon width={24} height={24} src={aeropay} />
-            </StyledAerocardTitleContainer>
-            <StyledAerocardTitleContainer>
-                <StyledAerocardUsername>{userData.name}</StyledAerocardUsername>
-                <p>{date.getMonth()}/{date.getFullYear().toLocaleString().slice(0,2)}</p>
-            </StyledAerocardTitleContainer>
+                <StyledAerocardWaves />
+                <StyledAerocardTitleContainer>
+                    <p>Aerocard</p>
+                    <StyledAerocardIcon width={24} height={24} src={aeropay} />
+                </StyledAerocardTitleContainer>
+                <StyledAerocardTitleContainer>
+                    <StyledAerocardUsername>{userData.name}</StyledAerocardUsername>
+                    <p>{date.getMonth()}/{date.getFullYear().toLocaleString().slice(2)}</p>
+                </StyledAerocardTitleContainer>
         </StyledAerocard>
     )
 }
