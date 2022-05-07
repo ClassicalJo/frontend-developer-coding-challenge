34de7db317e804752bc617838b925f047ba258d5
Joe <jb.barrientos@yahoo.com>
2022-05-07 18:19:07 -0300

    Refactored the component to avoid repetition

--- components/toasts/index.tsx
+++ components/toasts/index.tsx
@@ -1,41 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { SetStateAction } from 'react'
 import { ToastElement } from "../types";
-import { StyledErrorToast, StyledToast, StyledToastsContainer } from "./styles";
+import { StyledToastsContainer } from "./styles";
 import errorIcon from '../../assets/icons/system-error.svg'
 import successIcon from '../../assets/icons/system-success.svg'
 import crossActive from '../../assets/icons/cross-active.svg'
 import crossDefault from '../../assets/icons/cross-default.svg'
+import Toast from './Toast';
 interface AppProps {
     toasts: ToastElement[];
+    hide: Boolean;
 }
 
-export default function Toasts({ toasts }: AppProps): JSX.Element {
+export default function Toasts({ toasts, hide }: AppProps): JSX.Element {
     return (
-        <StyledToastsContainer>
-            {toasts.map((k, i) => {
-                switch (k.isError) {
-                    case false: return (
-                        <StyledToast
-                            key={`toast${i}`}
-                            item={k.message}
-                            icon={successIcon}
-                            crossActive={crossActive}
-                            crossDefault={crossDefault}
-                            isFirst={i === toasts.length - 1}
-                            display={i >= toasts.length - 3}
-                        />)
-                    default: return (
-                        <StyledErrorToast
-                            key={`errorToast${i}`}
-                            message={k.message}
-                            icon={errorIcon}
-                            crossActive={crossActive}
-                            crossDefault={crossDefault}
-                            isFirst={i === toasts.length - 1}
-                            display={i >= toasts.length - 3}
-                        />)
-                }
-            })}
+        <StyledToastsContainer hide={hide} >
+            {toasts.map((k, i) => (<Toast
+                key={`toast${i}`}
+                toast={k}
+                icon={successIcon}
+                crossActive={crossActive}
+                crossDefault={crossDefault}
+                first={i === toasts.length - 1}
+                display={i >= toasts.length - 3}
+                
+            />))}
         </StyledToastsContainer>
     )
 }
