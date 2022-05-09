f504c3187c8db338ed3f20fa9ef9f13d82c4f478
Joe <jb.barrientos@yahoo.com>
2022-05-09 19:11:49 -0300

    Adding additional styling to Logo

--- components/nav/styles.tsx
+++ components/nav/styles.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import breakpoints from '../breakpoints'
+import UnstyledNextImage from '../commonStyles/StyledNextImage'
 
 
 export const StyledNavBar = styled.div`
@@ -10,4 +12,13 @@ export const StyledNavBar = styled.div`
 
 export const StyledLink = styled.a`
     border-radius: 16px;
+`
+
+export const StyledLogo = styled(UnstyledNextImage)`
+    width: 38.77px;
+    height: 36px;
+    @media (min-width: ${breakpoints.desktop}){
+        width: 126px;
+        height: 48px;
+    }
 `
