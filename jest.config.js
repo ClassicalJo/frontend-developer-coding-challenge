b3028b1519e7ab4285b2515227c328002aabde14
Joe <jb.barrientos@yahoo.com>
2022-05-14 00:28:58 -0300

    Adding paths for testing imports

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,20 @@ const createJestConfig = nextJest({ dir: '.' })
 const customJestConfig = {
     testEnvironment: 'jsdom',
     clearMocks: true,
-    setupFilesAfterEnv: ['./components/setupTests.ts']
+    setupFilesAfterEnv: ['./tests/setupTests.ts'],
+    moduleDirectories: ["node_modules", "pages", 'components' ],
+    moduleNameMapper: {
+        "^@pages(.*)$": "<rootDir>/pages$1",
+        "^@browse(.*)$": "<rootDir>/components/browse$1",
+        "^@footer(.*)$": "<rootDir>/components/footer$1",
+        "^@hero(.*)$": "<rootDir>/components/hero$1",
+        "^@main(.*)$": "<rootDir>/components/main$1",
+        "^@nav(.*)$": "<rootDir>/components/nav$1",
+        "^@products(.*)$": "<rootDir>/components/products$1",
+        "^@toasts(.*)$": "<rootDir>/components/toasts$1",
+        "^@components(.*)$": "<rootDir>/components$1",
+    }
+
 }
 
 module.exports = createJestConfig(customJestConfig)
