66780ae669e102cd04da144669d5894e8c5b8bf2
Joe <jb.barrientos@yahoo.com>
2022-05-17 00:25:17 -0300

    Renamed properties for clarity, renamed method name to reflect api call method

--- components/api/UserHandler.ts
+++ components/api/UserHandler.ts
@@ -3,24 +3,24 @@ import { IPointsCharge } from "./PointsCharge"
 import { IUserFetch } from "./UserFetch"
 
 export class UserHandler {
-    points: string;
-    user: string;
+    pointsURL: string;
+    userURL: string;
     token: string;
     userFetch: IUserFetch;
     pointsCharge: IPointsCharge;
     constructor(points: string, user: string, token: string, userFetch: IUserFetch, pointsCharge: IPointsCharge) {
-        this.points = points
-        this.user = user
+        this.pointsURL = points
+        this.userURL = user
         this.token = token
         this.userFetch = userFetch
         this.pointsCharge = pointsCharge
     }
     handlePointCharge(req: NextApiRequest, res: NextApiResponse) {
         return new Promise<void>(resolve => {
-            let { amount } = JSON.parse(req.body)
-            this.pointsCharge.chargePoints(this.points, amount, this.token)
+            let body = JSON.parse(req.body)
+            this.pointsCharge.post(this.pointsURL, body.amount, this.token)
                 .then(data => res.status(200).json(data))
-                .catch(err => res.status(400).json(err))
+                .catch(error => res.status(400).json({ error }))
                 .finally(resolve)
         })
     }
@@ -32,7 +32,7 @@ export class UserHandler {
     }
     handleFetchUser(req: NextApiRequest, res: NextApiResponse) {
         return new Promise<void>(resolve => {
-            this.userFetch.get(this.user, this.token)
+            this.userFetch.get(this.userURL, this.token)
                 .then(data => res.json(data))
                 .catch(err => res.json(err))
                 .finally(resolve)
