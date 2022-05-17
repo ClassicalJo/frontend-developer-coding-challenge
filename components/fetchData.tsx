395393b2aa26412dc81c9d0cb006bfc7951b7a0a
Joe <jb.barrientos@yahoo.com>
2022-05-17 00:22:26 -0300

    Given a success type, fetchData returns either an object containing an error or the success type object

--- components/fetchData.tsx
+++ components/fetchData.tsx
@@ -1,15 +1,17 @@
 import authHeader from "./authHeader"
-import { FetchData } from "./types"
+type SuccessMessage<T> = T
+type ErrorMessage = { error: any }
+type Result<T> = SuccessMessage<T> | ErrorMessage
 
-export default async function fetchData(url: string, method: string, token: string): Promise<FetchData> {
+export default async function fetchData<T>(url: string, method: string, token: string): Promise<Result<T>> {
     let headers = authHeader(token)
     try {
         const res = await fetch(url, { headers, method })
         const data = await res.json()
+        if (data?.success === false) throw Error(data?.message)
         return data
     }
-    catch (err) {
-        let error = new Error("Error while fetching data on url " + url)
-        return { error: error.message }
+    catch (error) {
+        return { error }
     }
 }
