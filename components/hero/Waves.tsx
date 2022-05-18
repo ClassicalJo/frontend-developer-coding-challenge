0e7e549ed32774648391a3161453e093ac3c394e
Joe <jb.barrientos@yahoo.com>
2022-05-18 16:35:29 -0300

    Cleaned up the function to map 66% less components
    Decluttering the DOM and expanding the wave to better fit figma

--- components/hero/Waves.tsx
+++ components/hero/Waves.tsx
@@ -5,9 +5,9 @@ import useWaves from './useWaves'
 
 function Waves({ className }: StyledProplessComponent): JSX.Element {
     let totalArea = useWaves()
-    let waveMargin = 15
+    let waveMargin = 14.2
     let waveWidth = 650
-    let xOffSet = 2
+    let xOffSet = -100
     let yOffSet = 14
     let waveArray = new Array(Math.floor(totalArea / waveMargin) - 5).fill("")
 
@@ -18,15 +18,24 @@ function Waves({ className }: StyledProplessComponent): JSX.Element {
                 <symbol id="wave" viewBox='0 0 566 27'>
                     <image href={wave.src} x={0} y={0} />
                 </symbol>
+                <symbol id="fullscreen-wave" viewBox='0 0 1920 27'>
+                    <use xlinkHref='#wave' x={-700} y='0' transform="scale(1.15, 1)" />
+                    <use xlinkHref='#wave' x={-135} y='0' transform="scale(1.15, 1)" />
+                    <use xlinkHref='#wave' x={430} y='0' transform="scale(1.15, 1)" />
+                </symbol>
             </defs>
-            {waveArray.map((k, i) => (
-                <React.Fragment key={`wave${i}`}>
-                    <use xlinkHref='#wave' x="0" y={waveMargin * i + yOffSet} width={waveWidth} />
-                    <use xlinkHref='#wave' x={waveWidth - xOffSet} y={waveMargin * i + yOffSet} width={waveWidth} />
-                    <use xlinkHref='#wave' x={waveWidth * 2 - xOffSet * 2} y={waveMargin * i + yOffSet} width={waveWidth} />
-                </React.Fragment>
-            ))}
 
+            <g data-testid="wave-container">
+                {waveArray.map((k, i) => (
+                    <use
+                        key={`wave${i}`}
+                        xlinkHref='#fullscreen-wave'
+                        x={0}
+                        y={waveMargin * i + yOffSet}
+                        width="1920"
+                    />
+                ))}
+            </g>
         </svg>
     )
 }
