ec6c3ca4bce5ba9ec576eac2d16b208d8fb7c525
Joe <jb.barrientos@yahoo.com>
2022-05-24 22:21:45 -0300

    Refactor: Moved hook into own file for clarity and testing. Adding button role to link

--- components/nav/Logo.tsx
+++ components/nav/Logo.tsx
@@ -1,24 +1,9 @@
-
-import Image, { StaticImageData } from 'next/image'
-import { useCallback, useEffect, useState } from 'react'
-import aerolabLogo1 from '../../assets/icons/aerolab-logo-1.svg'
-import aerolabLogo2 from '../../assets/icons/aerolab-logo-2.svg'
 import { StyledLink, StyledLogo } from './styles'
-
+import useLogo from './useLogo'
 export default function Logo(): JSX.Element {
-    let isDesktop = useCallback(() => window.innerWidth >= 1920, [])
-    let [src, setSrc] = useState<StaticImageData>(isDesktop() ? aerolabLogo1 : aerolabLogo2)
-    useEffect(() => {
-        function resize() {
-            if (isDesktop() && src !== aerolabLogo1) setSrc(aerolabLogo1)
-            else if (!isDesktop() && src !== aerolabLogo2) setSrc(aerolabLogo2)
-        }
-        resize()
-        window.addEventListener('resize', resize)
-        return () => window.removeEventListener('resize', resize)
-    }, [isDesktop, src])
+    let src = useLogo()
     return (
-        <StyledLink href="https://aerolab.us/">
+        <StyledLink href="https://aerolab.us/" role='button' title="Link to Aerolab's homepage">
             <StyledLogo
                 width={src.width}
                 height={src.height}
